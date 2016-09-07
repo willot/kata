c852d51f5b172f3ed007ebb922a938b68bf36c16
willot <vianney.willot@gmail.com>
2016-09-06 22:17:14 -0400

    handle negative number

--- src/ConvertArabicNumeralToRoman.java
+++ src/ConvertArabicNumeralToRoman.java
@@ -4,12 +4,16 @@ public class ConvertArabicNumeralToRoman {
 	public String convert(int arabicNumber) {
 		String romanNumeral = "";
 		
+		if(arabicNumber >0) {
+		
 		while (arabicNumber>0) {
 			arabicNumber --;
 			romanNumeral += "I";
 					}
-		
 		return romanNumeral;
+		}
+		
+		return "We only accept positive number";
 	}
 
 }
--- src/ConvertArabicNumeralToRomanTest.java
+++ src/ConvertArabicNumeralToRomanTest.java
@@ -5,9 +5,11 @@ public class ConvertArabicNumeralToRomanTest extends TestCase {
 	
 	public void testSimpleConversion() {
 		ConvertArabicNumeralToRoman converter = new ConvertArabicNumeralToRoman();
+		String errorMessage = "We only accept positive number"; 
 		
 		assertEquals("I", converter.convert(1));
 		assertEquals("II", converter.convert(2));
+		assertEquals(errorMessage, converter.convert(-4));
 	}
 
 }

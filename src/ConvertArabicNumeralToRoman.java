0acf9f2f2cf83d86b0693847a4dcfc2f83fd70d0
willot <vianney.willot@gmail.com>
2016-09-06 22:30:57 -0400

    implement simple case statement to handle small numbers

--- src/ConvertArabicNumeralToRoman.java
+++ src/ConvertArabicNumeralToRoman.java
@@ -6,10 +6,62 @@ public class ConvertArabicNumeralToRoman {
 		
 		if(arabicNumber >0) {
 		
-		while (arabicNumber>0) {
-			arabicNumber --;
-			romanNumeral += "I";
-					}
+			switch (arabicNumber) {
+			case 1:
+				romanNumeral= "I";
+			break;
+		
+			case 2:
+				while (arabicNumber>0) {
+				arabicNumber --;
+				romanNumeral += "I";
+						}
+			break;
+			case 3:
+				while (arabicNumber>0) {
+				arabicNumber --;
+				romanNumeral += "I";
+						}
+			break;
+			case 4:
+				romanNumeral += "IV";
+			break;
+			
+			case 5:
+				romanNumeral += "V";
+			break;
+			
+			case 6:
+				romanNumeral = "V";
+				arabicNumber = arabicNumber -5;
+				while (arabicNumber >0) {
+					arabicNumber --;
+					romanNumeral += "I";
+							}
+			break;
+			
+			case 7:
+				romanNumeral = "V";
+				arabicNumber = arabicNumber -5;
+				while (arabicNumber >0) {
+					arabicNumber --;
+					romanNumeral += "I";
+							}
+			break;
+			
+			case 8:
+				romanNumeral = "V";
+				arabicNumber = arabicNumber -5;
+				while (arabicNumber >0) {
+					arabicNumber --;
+					romanNumeral += "I";
+							}
+			break;
+		
+			default:
+				break;
+			}
+		
 		return romanNumeral;
 		}
 		
--- src/ConvertArabicNumeralToRomanTest.java
+++ src/ConvertArabicNumeralToRomanTest.java
@@ -3,13 +3,19 @@ import junit.framework.TestCase;
 
 public class ConvertArabicNumeralToRomanTest extends TestCase {
 	
-	public void testSimpleConversion() {
+	public void testConversionUnder4() {
 		ConvertArabicNumeralToRoman converter = new ConvertArabicNumeralToRoman();
 		String errorMessage = "We only accept positive number"; 
 		
 		assertEquals("I", converter.convert(1));
 		assertEquals("II", converter.convert(2));
 		assertEquals(errorMessage, converter.convert(-4));
+	}
+	
+	public void testConversionbetween4And10() {
+		ConvertArabicNumeralToRoman converter = new ConvertArabicNumeralToRoman();
+		assertEquals("V", converter.convert(5));
+		assertEquals("VII", converter.convert(7));
 	}
 
 }

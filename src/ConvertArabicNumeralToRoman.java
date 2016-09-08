6193bca697082dfa76bd7e1502cb8910be485236
willot <vianney.willot@gmail.com>
2016-09-08 19:31:48 -0400

    cleaned and refactor production and tests

--- src/ConvertArabicNumeralToRoman.java
+++ src/ConvertArabicNumeralToRoman.java
@@ -5,162 +5,47 @@ import java.util.Iterator;
 
 public class ConvertArabicNumeralToRoman {
 
-	private String romanNumeral;
-	private String romanNumericalFor1 = "I";
-	private String romanNumericalFor5 = "V";
-	private String romanNumericalFor10 = "X";
-	private String romanNumericalFor50 = "L";
-	private String romanNumericalFor100 = "C";
-
-	public String convert(int arabicNumber) {
-		romanNumeral ="";
-		
-		if (arabicNumber>10) {
-			int tensDigitValue = arabicNumber/10;
-			int tensDigitValueMinus5 = arabicNumber/10-5;
-			int onesDigitValue = arabicNumber%10;
-			int onesDigitValueMinus5 = onesDigitValue -5;
-			
-			String romanNumeralValueForTenstDigit ="";
-			
-			switch (tensDigitValue) {
-			case 1: case 2: case 3:
-				
-				
-				romanNumeralValueForTenstDigit = addTheRightNumberOfX(tensDigitValue, romanNumeralValueForTenstDigit);
-				
-				convertOnesDigitIntoRomanNumeral(onesDigitValue, romanNumericalFor5,
-						romanNumericalFor10, onesDigitValueMinus5);
-				romanNumeral = romanNumeralValueForTenstDigit + romanNumeral;
-				return romanNumeral;
-				
-			case 4: 
-			
-				romanNumeralValueForTenstDigit = romanNumericalFor10 + romanNumericalFor50;
-				
-				convertOnesDigitIntoRomanNumeral(onesDigitValue, romanNumericalFor5,
-						romanNumericalFor10, onesDigitValueMinus5);
-				romanNumeral = romanNumeralValueForTenstDigit + romanNumeral;
-				return romanNumeral;
-				
-				
-			case 5: 
-			
-				romanNumeralValueForTenstDigit = romanNumericalFor50;
-				
-				convertOnesDigitIntoRomanNumeral(onesDigitValue, romanNumericalFor5,
-						romanNumericalFor10, onesDigitValueMinus5);
-				romanNumeral = romanNumeralValueForTenstDigit + romanNumeral;
-				return romanNumeral;
-				
-			case 6: case 7: case 8:
-				
-				romanNumeralValueForTenstDigit = addTheRightNumberOfX(tensDigitValueMinus5, romanNumericalFor50);
-				
-				convertOnesDigitIntoRomanNumeral(onesDigitValue, romanNumericalFor5,
-						romanNumericalFor10, onesDigitValueMinus5);
-						
-				romanNumeral = romanNumeralValueForTenstDigit + romanNumeral;
-				return romanNumeral;
-				
-			case 9:
-					
-				romanNumeralValueForTenstDigit = romanNumericalFor10 + romanNumericalFor100;
-				
-				convertOnesDigitIntoRomanNumeral(onesDigitValue, romanNumericalFor5,
-						romanNumericalFor10, onesDigitValueMinus5);
-						
-				romanNumeral = romanNumeralValueForTenstDigit + romanNumeral;
-				return romanNumeral;
-			}
-			
-			
-			
-			
-		}
-		
-		if(arabicNumber >0) {
-			int arabicNumberMinus5 = arabicNumber - 5;
-		
-			convertOnesDigitIntoRomanNumeral(arabicNumber, romanNumericalFor5,
-					romanNumericalFor10, arabicNumberMinus5);
-		
-		return romanNumeral;
-		}
-		
-		return "We only accept positive number";
-	}
-
-	private void convertOnesDigitIntoRomanNumeral(int arabicNumber,
-			String romanNumericalFor5, String romanNumericalFor10,
-			int arabicNumberMinus5) {
-		switch (arabicNumber) {
-		case 1: case 2: case 3:
-			romanNumeral= addTheRightNumberOfI(arabicNumber, romanNumeral);
-		break;
-			
-		case 4:
-			romanNumeral = "IV";
-		break;
-		
-		case 5:
-			romanNumeral = romanNumericalFor5;
-		break;
-		
-		case 6: case 7: case 8:
-			romanNumeral=addTheRightNumberOfI(arabicNumberMinus5, romanNumericalFor5);
-		break;
-		
-		case 9:
-			romanNumeral= romanNumericalFor1 + romanNumericalFor10;
-			break;
-			
-		case 10:
-			romanNumeral=romanNumericalFor10;
-			break;
-
-		default:
-			break;
-		}
-	}
+	private String romanNumeral ="";
+	private String[] romanNumberReference= {"M","CM" ,"D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+	private Integer[] arabicNumberReferences= {1000,900,500,400,100,90,50,40,10,9,5,4,1};
 	
-	 public String addTheRightNumberOfI (int arabicNumber, String romanNumeral) {
-		while (arabicNumber >0) {
-			arabicNumber --;
-			romanNumeral += "I";
-					}
-		return romanNumeral;
+	 
+	 public String convert(int arabicNumber) {
+		 romanNumeral ="";
+		 int remainingOfArabicNumber = arabicNumber;
+	 
+		 if (arabicNumber > 3999) {
+			 romanNumeral = "Reach possible limit";
+			 return romanNumeral;
+		 }
+		 if (arabicNumber < 0) {
+			 romanNumeral = "We only accept positive number";
+			 return romanNumeral;
+		 }
 
-	}
-	 
-	 public String addTheRightNumberOfX (int arabicNumber, String romanNumeral) {
-			while (arabicNumber >0) {
-				arabicNumber --;
-				romanNumeral += "X";
-						}
-			return romanNumeral;
-
-		}
-	 
-	 
-	 
-	 public String convertVersion2(int arabicNumber) {
-		 
-		 String[] romanNumberReference= {"C","L","X","V","I"};
-		 Integer[] arabicNumberReferences= {100,50,10,5,1};
-
-		
-			 for (int index = 0; index < arabicNumberReferences.length; index++) {
-				 int remainingOfArabicNumber = arabicNumberReferences[index]/arabicNumber;
-				 System.out.println(remainingOfArabicNumber);
+		for (int index = 0; index < arabicNumberReferences.length; index++) {
+				 				 
+				 int quotientOfArabicNumber = remainingOfArabicNumber/arabicNumberReferences[index];
 					
-				 if(remainingOfArabicNumber == 1 ) {
+				 if(arabicNumber == arabicNumberReferences[index] ) {
 					 romanNumeral = romanNumberReference[index];
 					 return romanNumeral;
-			}
-		 		 
+				 }
+				 
+				 if (quotientOfArabicNumber>0) {
+					 while (quotientOfArabicNumber >0) {
+						 quotientOfArabicNumber --;
+						 romanNumeral += romanNumberReference[index];
+						 remainingOfArabicNumber = remainingOfArabicNumber - arabicNumberReferences[index];
+					 }
+				 }
+				 
+				 if (remainingOfArabicNumber <= 0) {
+					 return romanNumeral;
+				 }
+				 
+
 		 }
-		 
 		 return romanNumeral;
 	 }
 
--- src/ConvertArabicNumeralToRomanTest.java
+++ src/ConvertArabicNumeralToRomanTest.java
@@ -3,22 +3,45 @@ import junit.framework.TestCase;
 
 public class ConvertArabicNumeralToRomanTest extends TestCase {
 	
-	public void testConversionUnder4() {
+	public void testConversionErrorsCases() {
 		ConvertArabicNumeralToRoman converter = new ConvertArabicNumeralToRoman();
-		String errorMessage = "We only accept positive number"; 
+		String errorMessageNegativeNumbers = "We only accept positive number"; 
+		String errorMessageNumberAbove3999 = "Reach possible limit"; 
 		
-		assertEquals("I", converter.convert(1));
-		assertEquals("II", converter.convert(2));
-		assertEquals("III", converter.convert(3));
-		assertEquals(errorMessage, converter.convert(-4));
+		assertEquals(errorMessageNegativeNumbers, converter.convert(-4));
+		assertEquals(errorMessageNumberAbove3999, converter.convert(4000));
+		assertEquals(errorMessageNumberAbove3999, converter.convert(4159));
 	}
 	
-	public void testConversionbetween4And10() {
+	public void testConversionOfBasicNumbers() {
 		ConvertArabicNumeralToRoman converter = new ConvertArabicNumeralToRoman();
+		assertEquals("I", converter.convert(1));
 		assertEquals("V", converter.convert(5));
+		assertEquals("X", converter.convert(10));
+		assertEquals("L", converter.convert(50));
+		assertEquals("C", converter.convert(100));
+		assertEquals("D", converter.convert(500));
+		assertEquals("M", converter.convert(1000));
+	}
+	
+	public void testBetterConversionOf4And() {
+		ConvertArabicNumeralToRoman converter = new ConvertArabicNumeralToRoman();
+		assertEquals("IV", converter.convert(4));
+		assertEquals("IX", converter.convert(9));
+		assertEquals("XL", converter.convert(40));
+		assertEquals("XC", converter.convert(90));
+		assertEquals("CD", converter.convert(400));
+		assertEquals("CM", converter.convert(900));
+	}	
+	
+	
+	public void testConversionbetween2And10() {
+		ConvertArabicNumeralToRoman converter = new ConvertArabicNumeralToRoman();
+		assertEquals("II", converter.convert(2));
+		assertEquals("III", converter.convert(3));
+		assertEquals("VI", converter.convert(6));
 		assertEquals("VII", converter.convert(7));
 		assertEquals("IX", converter.convert(9));
-		assertEquals("X", converter.convert(10));
 	}
 
 	
@@ -35,32 +58,26 @@ public class ConvertArabicNumeralToRomanTest extends TestCase {
 		assertEquals("LXXXIV", converter.convert(84));
 		assertEquals("XCVI", converter.convert(96));
 		assertEquals("XCIX", converter.convert(99));
-		
-		
 	}
 	
-	
-//	public void testConversionbetween100And1000() {
-//		ConvertArabicNumeralToRoman converter = new ConvertArabicNumeralToRoman();
-//		assertEquals("CI", converter.convert(101));
-//		assertEquals("CLXX", converter.convert(170));
-//		assertEquals("CCXXIV", converter.convert(224));
-//		assertEquals("CCCXXXIX", converter.convert(339));
-//		assertEquals("CDXL", converter.convert(440));
-//		assertEquals("DLXXXIX", converter.convert(589));
-//		assertEquals("DCXCIV", converter.convert(694));
-//		assertEquals("DCCXIII", converter.convert(713));
-//		assertEquals("DCCCXCV", converter.convert(895));
-//		assertEquals("CMXVI", converter.convert(916));
-//		assertEquals("XCIX", converter.convert(999));
-//	}
-	
-	public void testBetterConversion() {
+	public void testConversionbetween100And1000() {
 		ConvertArabicNumeralToRoman converter = new ConvertArabicNumeralToRoman();
-		assertEquals("V", converter.convertVersion2(5));
-		assertEquals("X", converter.convertVersion2(10));
-		assertEquals("L", converter.convertVersion2(50));
+		assertEquals("CI", converter.convert(101));
+		assertEquals("CLXX", converter.convert(170));
+		assertEquals("CCXXIV", converter.convert(224));
+		assertEquals("CCCXXXIX", converter.convert(339));
+		assertEquals("CDXL", converter.convert(440));
+		assertEquals("DLXXXIX", converter.convert(589));
+		assertEquals("DCXCIV", converter.convert(694));
+		assertEquals("DCCXIII", converter.convert(713));
+		assertEquals("DCCCXCV", converter.convert(895));
+		assertEquals("CMXVI", converter.convert(916));
+		assertEquals("CMXCIX", converter.convert(999));
+		assertEquals("MXXXV", converter.convert(1035));
+		assertEquals("MCLIX", converter.convert(1159));
+		assertEquals("MCMXXI", converter.convert(1921));
+		assertEquals("MMDCLXXXVII", converter.convert(2687));
+		assertEquals("MMMCMXCIX", converter.convert(3999));
 		
 	}
-	
 }
